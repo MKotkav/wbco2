feb968aeb52ff991d56d390b15c5c267c0ce015b
Mikael Kotkavuori <mikael.kotkavuori@gmail.com>
2018-12-15 20:16:16 +0200

    Migrate from create-react-app-buildpack to Node server

---
+++ react-ui/build/precache-manifest.98a2accd3d37facb77e01e57974f220a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "388f6f10473b547f9cfb",
+    "url": "/static/js/main.388f6f10.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "388f6f10473b547f9cfb",
+    "url": "/static/css/main.b04fbd9e.chunk.css"
+  },
+  {
+    "revision": "0438dbea37dee0db4a76d8e469dd83b5",
+    "url": "/index.html"
+  }
+];
